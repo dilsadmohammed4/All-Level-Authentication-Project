58a2f16bdfb51cd1085ebadcfc44bfb6513693ff
Dilsad Mohammed <dilsadmohammed4@gmail.com>
2021-05-22 11:01:05 +0530

    Initial commit and add .env file

---
+++ app.js
@@ -0,0 +1,94 @@
+//jshint esversion:6
+require('dotenv').config()
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(express.static("public"));
+
+mongoose.connect('mongodb://localhost:27017/userDB', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+});
+
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+//const secret = process.env.SECRET;
+
+userSchema.plugin(encrypt, {
+    secret: process.env.SECRET,
+    encryptedFields: ['password']
+});
+
+const User = new mongoose.model("User", userSchema);
+
+
+
+app.get('/', (req, res) => {
+    res.render("home")
+})
+
+app.get('/login', (req, res) => {
+    res.render("login")
+})
+app.post('/login', (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    User.findOne({
+        email: username
+    }, (err, foundUser) => {
+        if (err) {
+            console.log(err);
+        } else {
+            if (foundUser) {
+                if (foundUser.password === password) {
+                    res.render("secrets");
+                } else {
+                    res.render("login");
+                }
+            }
+        }
+    })
+})
+app.get('/register', (req, res) => {
+    res.render("register")
+})
+app.post('/register', (req, res) => {
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    });
+    newUser.save((err) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("secrets")
+        }
+    });
+});
+
+
+app.listen(3000, () => {
+    console.log("Server started...");
+});
